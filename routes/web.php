411990e43299084314d840afae0854b61ee780bb
Ciscom224 <mamadouibncisse@gmail.com>
2021-07-12 20:07:51 +0100

    ajout du fichier web.php contenant les routes pour la section1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EtudiantController;
+use App\Http\Controllers\Section2Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,16 @@ Route::post('auth/check',[EtudiantController::class,'check'])->name('check');
 //Pages routes
 Route::get('/home',[EtudiantController::class,'home'])->name('home')->middleware('isNotLogged');
 Route::get('/home/logout',[EtudiantController::class,'logout'])->name('logout')->middleware('isNotLogged');
-Route::get('/home/section/{id}',[EtudiantController::class,'getAnSection'])->name('sections')->middleware('isNotLogged');
+Route::get('/home/section/{id}',[EtudiantController::class,'section'])->name('sections')->middleware('isNotLogged');
+//section1 routes
+Route::get('/home/section/1/apprendre',function(){ return view('section1/exercice'); })->name('apprendre');
+Route::get('/home/section/1/chalenge',function(){ return view('section1/chalenge'); })->name('chalenge');
+Route::get('/home/section/1/',function(){ return view('home/sectionContent');})->name('section1');
+//section2 routes
+Route::get('home/section/{id}/exercice1',[Section2Controller::class,'exercice1'])->name('exercice1')->middleware('isNotLogged');
+Route::get('/',function(){
+    return view('index');
+});
+Route::get('/score/add',[EtudiantController::class,'addSectionScore']);
+Route::get('/vie/get',[EtudiantController::class,'etudiantVieInfo']);
+
